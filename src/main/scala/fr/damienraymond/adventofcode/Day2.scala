3f5db5eb25d8977ce948ad199f7cb6aa02c22e61
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2021-12-02 15:53:38 +0000

    Day 2 part 2

--- src/main/scala/fr/damienraymond/adventofcode/Day2.scala
+++ src/main/scala/fr/damienraymond/adventofcode/Day2.scala
@@ -2,6 +2,7 @@ package fr.damienraymond.adventofcode
 
 import cats.effect.{IO, Resource}
 import fr.damienraymond.ReadFileUtil
+import fr.damienraymond.adventofcode.Coordinates.{Coordinates, CoordinatesPart1, CoordinatesPart2}
 
 object Day2 {
 
@@ -15,19 +16,6 @@ object Day2 {
     case class Up(value: Int) extends Command
   }
 
-  case class Coordinates(horizontal: Int, depth: Int) {
-    def forward(value: Int): Coordinates =
-      copy(horizontal = horizontal + value)
-    def down(value: Int): Coordinates =
-      copy(depth = depth + value)
-    def up(value: Int): Coordinates =
-      copy(depth = depth - value)
-  }
-
-  object Coordinates {
-    val initial: Coordinates = Coordinates(0, 0)
-  }
-
   def parse(input: String): Option[Command] =
     input match {
       case s"forward ${value}" => value.toIntOption.map(Command.Forward)
@@ -35,31 +23,81 @@ object Day2 {
       case s"up ${value}" => value.toIntOption.map(Command.Up)
     }
 
-  def applyCommands(coordinates: Coordinates, command: Command): Coordinates = command match {
-    case Command.Forward(value) => coordinates.forward(value)
-    case Command.Down(value) => coordinates.down(value)
-    case Command.Up(value) => coordinates.up(value)
-  }
-
-  def applyCommands(initialCoordinates: Coordinates)(command: LazyList[Command]): Coordinates =
-    command.foldLeft(initialCoordinates)(applyCommands)
-
   def coordinateToProblemResult(coordinates: Coordinates): Int =
     coordinates match {
-      case Coordinates(horizontal, depth) => horizontal * depth
+      case CoordinatesPart1(horizontal, depth) => horizontal * depth
+      case CoordinatesPart2(aim, horizontal, depth) => horizontal * depth
     }
 
+  def applyCommands(initialCoordinates: Coordinates)(commands: LazyList[Command]): Coordinates = {
+    def applyCommand(coordinates: Coordinates, command: Command): Coordinates =
+      command match {
+        case Command.Forward(value) => coordinates.forward(value)
+        case Command.Down(value) => coordinates.down(value)
+        case Command.Up(value) => coordinates.up(value)
+      }
 
-  def day2(input: LazyList[String]): Int = {
+    commands.foldLeft(initialCoordinates)(applyCommand)
+  }
+
+  def program(initialCoordinates: Coordinates)(input: LazyList[String]): Int = {
     val commands = input.flatMap(parse)
-    val coordinates = applyCommands(Coordinates.initial)(commands)
+    val coordinates = applyCommands(initialCoordinates)(commands)
     coordinateToProblemResult(coordinates)
   }
 
+  def day2Part1(input: LazyList[String]): Int = program(CoordinatesPart1.initial)(input)
+
+  def day2Part2(input: LazyList[String]): Int = program(CoordinatesPart2.initial)(input)
 
   val day2Input: Resource[IO, LazyList[String]] =
     ReadFileUtil.readFileLine("day2.txt")
 
+
+}
+
+object Coordinates {
+
+  trait Coordinates {
+    def forward(value: Int): Coordinates
+
+    def down(value: Int): Coordinates
+
+    def up(value: Int): Coordinates
+  }
+
+  case class CoordinatesPart1(horizontal: Int, depth: Int) extends Coordinates {
+    def forward(value: Int): CoordinatesPart1 =
+      copy(horizontal = horizontal + value)
+
+    def down(value: Int): CoordinatesPart1 =
+      copy(depth = depth + value)
+
+    def up(value: Int): CoordinatesPart1 =
+      copy(depth = depth - value)
+  }
+
+  object CoordinatesPart1 {
+    val initial: CoordinatesPart1 = CoordinatesPart1(0, 0)
+  }
+
+  case class CoordinatesPart2(aim: Int, horizontal: Int, depth: Int) extends Coordinates {
+    def forward(value: Int): CoordinatesPart2 =
+      copy(
+        horizontal = horizontal + value,
+        depth = depth + aim * value
+      )
+
+    def down(value: Int): CoordinatesPart2 =
+      copy(aim = aim + value)
+
+    def up(value: Int): CoordinatesPart2 =
+      copy(aim = aim - value)
+  }
+
+  object CoordinatesPart2 {
+    val initial: CoordinatesPart2 = CoordinatesPart2(0, 0, 0)
+  }
 
 }
 
--- src/test/scala/fr/damienraymond/adventofcode/Day2Spec.scala
+++ src/test/scala/fr/damienraymond/adventofcode/Day2Spec.scala
@@ -2,51 +2,56 @@ package fr.damienraymond.adventofcode
 
 import cats.effect.IO
 import cats.effect.unsafe.implicits.global
-import fr.damienraymond.adventofcode.Day2.{Command, Coordinates}
+import fr.damienraymond.adventofcode.Coordinates.{Coordinates, CoordinatesPart1, CoordinatesPart2}
+import fr.damienraymond.adventofcode.Day2.Command
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
 class Day2Spec extends AnyFlatSpec with Matchers {
 
-  private val applyCommands: LazyList[Command] => Coordinates = Day2.applyCommands(Coordinates.initial)
+  private val applyCommandsPart1: LazyList[Command] => Coordinates =
+    Day2.applyCommands(CoordinatesPart1.initial)
+
+  private val applyCommandsPart2: LazyList[Command] => Coordinates =
+    Day2.applyCommands(CoordinatesPart2.initial)
 
   it should "move the submarine forward by 10" in {
-    applyCommands(LazyList(Command.Forward(10))) should be (Coordinates(10, 0))
+    applyCommandsPart1(LazyList(Command.Forward(10))) should be (CoordinatesPart1(10, 0))
   }
 
   it should "move the submarine forward by 5" in {
-    applyCommands(LazyList(Command.Forward(5))) should be (Coordinates(5, 0))
+    applyCommandsPart1(LazyList(Command.Forward(5))) should be (CoordinatesPart1(5, 0))
   }
 
   it should "increase the depth of the sub by 4" in {
-    applyCommands(LazyList(Command.Down(4))) should be (Coordinates(0, 4))
+    applyCommandsPart1(LazyList(Command.Down(4))) should be (CoordinatesPart1(0, 4))
   }
 
   it should "increase the depth of the sub by 6" in {
-    applyCommands(LazyList(Command.Down(6))) should be (Coordinates(0, 6))
+    applyCommandsPart1(LazyList(Command.Down(6))) should be (CoordinatesPart1(0, 6))
   }
 
   it should "decrease the depth of the sub by 6" in {
-    applyCommands(LazyList(Command.Up(6))) should be (Coordinates(0, -6))
+    applyCommandsPart1(LazyList(Command.Up(6))) should be (CoordinatesPart1(0, -6))
   }
 
   it should "decrease the depth of the sub by 9" in {
-    applyCommands(LazyList(Command.Up(9))) should be (Coordinates(0, -9))
+    applyCommandsPart1(LazyList(Command.Up(9))) should be (CoordinatesPart1(0, -9))
   }
 
   it should "apply multiple commands" in {
-    applyCommands(LazyList(
+    applyCommandsPart1(LazyList(
       Command.Forward(5),
       Command.Down(5),
       Command.Forward(8),
       Command.Up(3),
       Command.Down(8),
       Command.Forward(2)
-    )) should be (Coordinates(15, 10))
+    )) should be (CoordinatesPart1(15, 10))
   }
 
   it should "return the right result" in {
-    Day2.coordinateToProblemResult(Coordinates(15, 10)) should be (150)
+    Day2.coordinateToProblemResult(CoordinatesPart1(15, 10)) should be (150)
   }
 
   it should "parse a forward 10 command" in {
@@ -65,8 +70,8 @@ class Day2Spec extends AnyFlatSpec with Matchers {
     Day2.parse("up 2") should contain (Command.Up(2))
   }
 
-  it should "return 150 for the all example" in {
-    Day2.day2(
+  it should "return 150 for the all example for part1" in {
+    Day2.day2Part1(
       """forward 5
         |down 5
         |forward 8
@@ -77,9 +82,74 @@ class Day2Spec extends AnyFlatSpec with Matchers {
   }
 
 
-  it should "return return the actual result for the all input" in {
-    val res = Day2.day2Input.use(lines => IO(Day2.day2(lines))).unsafeRunSync()
-    println(res)
+  it should "return return the actual result for the all input part1" in {
+    val res = Day2.day2Input.use(lines => IO(Day2.day2Part1(lines))).unsafeRunSync()
+    println(s"Part 1 result ${res}")
   }
+
+  it should "increase the aim by 5" in {
+    applyCommandsPart2(LazyList(Command.Down(5))) should be (CoordinatesPart2(5, 0, 0))
+  }
+
+  it should "increase the aim by 5 + 2" in {
+    applyCommandsPart2(LazyList(
+      Command.Down(5),
+      Command.Down(2),
+    )) should be (CoordinatesPart2(7, 0, 0))
+  }
+
+  it should "decrease the aim by 9" in {
+    applyCommandsPart2(LazyList(Command.Up(9))) should be (CoordinatesPart2(-9, 0, 0))
+  }
+
+  it should "decrease the aim by 9+2" in {
+    applyCommandsPart2(LazyList(
+      Command.Up(9),
+      Command.Up(2)
+    )) should be (CoordinatesPart2(-11, 0, 0))
+  }
+
+  it should "forward the sub by 7" in {
+    applyCommandsPart2(LazyList(Command.Forward(7))) should be (CoordinatesPart2(0, 7, 0))
+  }
+
+  it should "forward the sub by 7+5" in {
+    applyCommandsPart2(LazyList(
+      Command.Forward(7),
+      Command.Forward(5),
+    )) should be (CoordinatesPart2(0, 12, 0))
+  }
+
+  it should "forward the sub by 7 with aim of 5" in {
+    applyCommandsPart2(LazyList(
+      Command.Down(5),
+      Command.Forward(7),
+    )) should be (CoordinatesPart2(5, 7, 5 * 7))
+  }
+
+  it should "forward the sub by 7 and 3 with aim of 5" in {
+    applyCommandsPart2(LazyList(
+      Command.Down(5),
+      Command.Forward(7),
+      Command.Forward(3),
+    )) should be (CoordinatesPart2(5, 7 + 3, 5 * 7 + 3 * 5))
+  }
+
+  it should "return 900 for the all example for part2" in {
+    Day2.day2Part2(
+      """forward 5
+        |down 5
+        |forward 8
+        |up 3
+        |down 8
+        |forward 2""".stripMargin.split("\n").to(LazyList)
+    ) should be (900)
+  }
+
+  it should "return return the actual result for the all input part2" in {
+    val res = Day2.day2Input.use(lines => IO(Day2.day2Part2(lines))).unsafeRunSync()
+    println(s"Part 2 result ${res}")
+  }
+
 
 }
