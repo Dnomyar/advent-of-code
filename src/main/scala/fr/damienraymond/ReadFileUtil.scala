daff9d820cff7caa99498a4ece18e0b2dfbe0753
Damien Raymond <damien.raymond@paddypowerbetfair.com>
2021-12-02 12:09:05 +0000

    Fix read file util

--- src/main/scala/fr/damienraymond/ReadFileUtil.scala
+++ src/main/scala/fr/damienraymond/ReadFileUtil.scala
@@ -7,7 +7,7 @@ import scala.io.Source
 object ReadFileUtil {
 
   def readFile(filename: String): Resource[IO, Iterator[String]] =
-    Resource.make(IO(Source.fromFile(filename)))(source => IO(source.close()))
+    Resource.make(IO(Source.fromResource(filename)))(source => IO(source.close()))
       .map(_.getLines())
 
   def readFileInts(filename: String): Resource[IO, LazyList[Int]] =
